0a8d7c4166d086c9eee12e73e876cd24c22462d3
almudena <almudena@almudena-Programmer.station>
2025-11-25 00:48:56 +0000

    25Nov

--- src/main/java/com/example/javamasterclass/strings/CharacterFrequencyCounter.java
+++ src/main/java/com/example/javamasterclass/strings/CharacterFrequencyCounter.java
@@ -1,5 +1,33 @@
 package com.example.javamasterclass.strings;
 
-public class CharacterFrequencyCounter {
+import java.util.Arrays;
 
+import com.example.javamasterclass.strings.utilities.ConsolePrompt;
+
+
+public class CharacterFrequencyCounter {
+	
+	public static int[] count (String s) {
+		
+		int [] freq = new int[256];
+		char [] charArray = s.toCharArray();
+		if (charArray.length>0) {
+			for(char c: charArray) {
+				freq[Character.toLowerCase(c)]++;
+			}
+		}
+		
+		return freq;
+		
+	}
+
+	public static void main (String [] args) {
+		String s = ConsolePrompt.readString("Inroduce a string so we can count character frequency");
+		int [] freq = count(s);
+		for(int i=0; i<freq.length; i++) {
+			if(freq[i]>0) {
+				System.out.println((char) i + " -> " + freq[i]); 
+			}
+		}
+	}
 }
--- src/main/java/com/example/javamasterclass/strings/SubStringSearch.java
+++ src/main/java/com/example/javamasterclass/strings/SubStringSearch.java
@@ -1,5 +1,40 @@
 package com.example.javamasterclass.strings;
 
+import com.example.javamasterclass.strings.utilities.ConsolePrompt;
+
 public class SubStringSearch {
+	
+	public static int search(String sentence, String pattern) {
+
+		char [] sentenceChars= sentence.toCharArray();
+		char [] patternChars= pattern.toCharArray();
+		int j=0;
+		int i=0;
+		
+		while(i<sentenceChars.length){
+			if(sentenceChars[i]==patternChars[j]) {
+				j++;
+				if(j==patternChars.length) {
+					return i-j;
+				}
+			}else if (j>0) {
+				j=0;
+				i--;
+				
+			}			
+			i++;
+			
+			
+		}
+		
+		return -1;	
+		
+	}
+	public static void main (String [] args) {
+		String sentence= ConsolePrompt.readString("Introduce a sentence");
+		String pattern= ConsolePrompt.readString("Introduce the string to search");
+		System.out.println(search(sentence, pattern));
+		
+	}
 
 }
